b8e46c05cc56f2293ff80786532facd3edc4c852
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-03-16 21:52:19 +0100

    Finished OLX parser

--- Scrapers/Model/Announcement.cs
+++ Scrapers/Model/Announcement.cs
@@ -1,4 +1,6 @@
-﻿namespace Scrapers.Model
+﻿using System;
+
+namespace Scrapers.Model
 {
     public class Announcement
     {
@@ -9,9 +11,11 @@
 
         public string Voivodeship { get; set; }
         public string City { get; set; }
+        public string District { get; set; }
         
         public int BasePrice { get; set; }
-        public int Rent { get; set; }
+        public float Rent { get; set; }
+        public float PricePerSquareMeter { get; set; }
         
         public bool IsFromDeveloper { get; set; }
         public bool IncludesFurniture { get; set; }
@@ -20,6 +24,8 @@
         public string Rooms { get; set; }
         public string Floor { get; set; }
         
-        public string CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        
+        public DateTime ScrapedAt { get; set; } = DateTime.Now;
     }
 }
--- Scrapers/Program.cs
+++ Scrapers/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using Scrapers.Writing;
-using ScrapySharp.Network;
-
-namespace Scrapers
+﻿namespace Scrapers
 {
     internal static class Program
     {
@@ -14,18 +8,8 @@ namespace Scrapers
             {
             // Writer = new FileWriter(Path.Combine("Scrapes", "Olx"))
             };
-            // scraper.Start();
-            // scraper.ScrapeOffers();
-
-            const string url = "https://www.olx.pl/oferta/mieszkanie-w-scislym-centrum-pierwsze-pietro-warminskiego-14-CID3-IDDO3BM.html";
-            var browser = new ScrapingBrowser()
-            {
-                AutoDetectCharsetEncoding = false,
-                Encoding = Encoding.UTF8
-            };
-
-            var offer = scraper.Parser.ParseOffer(browser.NavigateToPage(new Uri(url)).Html);
-            scraper.Writer.SaveOne(offer);
+            scraper.Start();
+            scraper.ScrapeOffers();
         }
     }
 }
