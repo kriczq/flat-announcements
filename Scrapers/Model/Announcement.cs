7fa0c4737e8c3167899333da829e6b46f505d224
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-06-21 11:04:00 +0200

    Added geo coordinates to announcement class

--- Scrapers/Model/Announcement.cs
+++ Scrapers/Model/Announcement.cs
@@ -15,6 +15,8 @@ namespace Scrapers.Model
         public string City { get; set; }
         public string District { get; set; }
         public string Street { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
 
         public float BasePrice { get; set; }
         public float Rent { get; set; }
